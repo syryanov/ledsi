e9f9ac9bb854ff542b5ba487c27bdea8f419654f
Anatoliy Syryanov <a.syryanov@yandex.ru>
2026-02-11 10:45:59 +0000

    Logout

---
+++ app/src/components/Logout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import useAuthStore from '@/stores/auth'
+import { useToast } from 'primevue/usetoast'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+
+const toast = useToast()
+
+const router = useRouter()
+
+const submit = () => {
+  authStore.logout(
+    () => {
+      router.push('/auth')
+    },
+    () => {
+      toast.add({ severity: 'error', summary: 'Ошибка!', detail: 'Попробуйте ещё раз', life: 3000 })
+    }
+  )
+}
+</script>
+
+
+<template>
+<div>
+  <div class="cursor-pointer w-fit" @click="submit">Выйти</div>
+</div>
+</template>
