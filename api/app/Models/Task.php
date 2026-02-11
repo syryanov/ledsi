60b9edeb147f6288eb52defd60f790f3ed11c4ff
Anatoliy Syryanov <a.syryanov@yandex.ru>
2026-02-11 10:48:00 +0000

    Модель Task

---
+++ api/app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'status',
+    ];
+
+    /**
+     * Получить пользователя, которому принадлежит задача.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
