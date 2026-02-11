ece4f5d01cca1f815ccd4ae0d981192f5a2c7083
Anatoliy Syryanov <a.syryanov@yandex.ru>
2026-02-11 10:42:47 +0000

    vue

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,10 +3,20 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import ToastService from 'primevue/toastservice'
+import './assets/style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+app.use(ToastService)
 
 app.mount('#app')
