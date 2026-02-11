595666836ba4c4877ed5a323a4b0f0b957239280
Anatoliy Syryanov <a.syryanov@yandex.ru>
2026-02-11 10:47:04 +0000

    Routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StatsController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', [TaskController::class, 'index']);
+        Route::get('/{id}', [TaskController::class, 'show']);
+        Route::post('/', [TaskController::class, 'create']);
+        Route::post('/{id}', [TaskController::class, 'update']);
+        Route::delete('/{id}', [TaskController::class, 'delete']);
+    });
+
+    Route::get('/stats', [StatsController::class, 'index']);
+
+    Route::get('/users', [UsersController::class, 'index']);
 });
